90fa5d5d243d77df7d068349799bb5eba4ba4ee6
Snehavalli08 <23203030@srcas.ac.in>
2024-07-01 11:58:30 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,9 @@
+class sample{
+  public static void main(String[]args)
+  {
+    System.out.println("hello");
+    System.out.println("to");
+    System.out.println("class");
+    System.out.println("devops");
+  }
+}
